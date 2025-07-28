8b497cb368b0a8092bdda543d77b5c7069f7ea4a
Sushant Tuladhar <susantuladhar90@gmail.com>
2025-07-28 23:32:34 +0545

    Add output: export for Netlify static deployment

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
-  },
-}
-
-module.exports = nextConfig 
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'github.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+        },
+      ],
+    },
+    output: 'export',  // <-- add this for static export
+  }
+  
+  module.exports = nextConfig
+  
